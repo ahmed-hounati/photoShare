397175b09e0022dcea56c05b9883f8a5879f0343
ahmed-hounati <hounatiahmed@gmail.com>
2025-01-13 21:10:40 +0100

    fc

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,89 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <View style={{ flex: 1, backgroundColor: Colors.black }}>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: '#fff',
+                    headerShadowVisible: false,
+                    tabBarStyle: {
+                        backgroundColor: Colors.grey,
+                        borderRadius: 15,
+                        margin: 15,
+                        borderTopWidth: 0
+                    }
+                }}>
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        headerShown: false,
+                        title: 'Home',
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="friends"
+                    options={{
+                        headerShown: false,
+                        title: 'Friends',
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'people' : 'people-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="middle"
+                    options={{
+                        tabBarLabel: '',
+                        headerShown: false,
+                        tabBarIcon: () => (
+                            <TouchableOpacity onPress={undefined}>
+                                <Image
+                                    source={require('../../assets/images/tabBar.png')}
+                                    style={styles.middleTabIcon}
+                                />
+                            </TouchableOpacity>
+                        ),
+                        tabBarStyle: {
+                            display: 'none',
+                        },
+                    }}
+                />
+                <Tabs.Screen
+                    name="inbox"
+                    options={{
+                        headerShown: false,
+                        title: 'Add',
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'add' : 'add-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="account"
+                    options={{
+                        headerShown: false,
+                        title: 'Account',
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'person' : 'person-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </View>
+    )
 }
+
+
+const styles = StyleSheet.create({
+    middleTabIcon: {
+        width: 80,
+        height: 150,
+    },
+});
